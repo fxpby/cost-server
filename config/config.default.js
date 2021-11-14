c11ea661d5346287f11ede3eb8de9cbcfd770491
fxpby <fxpby@outlook.com>
2021-11-15 00:08:45 +0800

    feat: add mysql config

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,35 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  // 白名单配置
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: [ '*' ],
+  };
+
+  exports.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '120.55.99.110',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '13352029252',
+      // 数据库名
+      database: 'egg-cost',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
