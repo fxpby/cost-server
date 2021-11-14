4acb1e02fed0f8fde1532f99672c95a73b750170
fxpby <fxpby@outlook.com>
2021-11-15 00:48:44 +0800

    feat: update user controller

--- app/controller/user.js
+++ app/controller/user.js
@@ -7,10 +7,23 @@ class UserController extends Controller {
     const { ctx } = this;
     const { username, password } = ctx.request.body;
 
+    // 判断用户名/密码是否为空
     if (!username || !password) {
       ctx.body = {
         code: 500,
         msg: '不能填写空值哦',
+        data: null,
+      };
+      return;
+    }
+
+    // 验证数据库内是否已经存在重名用户
+    const userInfo = await ctx.service.user.getUserByName(username);
+
+    if (userInfo && userInfo.id) {
+      ctx.body = {
+        code: 500,
+        msg: '帐户名已经被注册了，请换一个吧',
         data: null,
       };
       return;
