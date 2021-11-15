b7117e76d7c2824fa2bb29959d8788d863d844ef
fxpby <fxpby@outlook.com>
2021-11-15 23:09:23 +0800

    feat: add unit test for login method in user controller

--- test/app/controller/user.test.js
+++ test/app/controller/user.test.js
@@ -60,4 +60,61 @@ describe('test/app/controller/user.test.js', () => {
       });
   });
 
+  it('POST /api/user/login 登陆成功', () => {
+    return app.httpRequest()
+      .post('/api/user/login')
+      .send({
+        username: 'Olu',
+        password: '123456',
+      })
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, 200);
+        assert(res.data.length);
+      });
+  });
+
+  it('POST /api/user/login 账号或密码为空', () => {
+    return app.httpRequest()
+      .post('/api/user/login')
+      .send({
+        username: '',
+        password: '123456',
+      })
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, 500);
+      });
+  });
+
+  it('POST /api/user/login 账号不存在', () => {
+    return app.httpRequest()
+      .post('/api/user/login')
+      .send({
+        username: '我是不存在的账号',
+        password: '123456',
+      })
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, 500);
+      });
+  });
+
+  it('POST /api/user/login 密码不正确', () => {
+    return app.httpRequest()
+      .post('/api/user/login')
+      .send({
+        username: 'Olu',
+        password: '12345654321',
+      })
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, 500);
+      });
+  });
+
 });
