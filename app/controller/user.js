0a5a79b3d5b07b998e2d345d0884308d984ccbe2
fxpby <fxpby@outlook.com>
2021-11-15 08:19:24 +0800

    feat: update register controller and service

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const deafultAvatar = 'https://fxpby.oss-cn-beijing.aliyuncs.com/project/egg-cost/avatar.jpg';
 
 class UserController extends Controller {
   async register() {
@@ -28,6 +29,30 @@ class UserController extends Controller {
       };
       return;
     }
+
+    // 写入数据库
+    const result = await ctx.service.user.register({
+      username,
+      password,
+      signature: '今天天气不错，设置一下个性签名吧',
+      avatar: deafultAvatar,
+      ctime: new Date(),
+    });
+
+    if (result) {
+      ctx.body = {
+        code: 200,
+        msg: '注册成功',
+        data: null,
+      };
+    } else {
+      ctx.body = {
+        code: 500,
+        msg: '注册失败',
+        data: null,
+      };
+    }
+
   }
 }
 
--- app/service/user.js
+++ app/service/user.js
@@ -3,6 +3,7 @@
 const Service = require('egg').Service;
 
 class UserService extends Service {
+
   // 通过用户名获取用户信息
   async getUserByName(username) {
     const { app } = this;
@@ -10,6 +11,18 @@ class UserService extends Service {
       const result = await app.mysql.get('user', { username });
       return result;
     } catch (error) {
+      return null;
+    }
+  }
+
+  // 注册
+  async register(params) {
+    const { app } = this;
+    try {
+      const result = await app.mysql.insert('user', params);
+      return result;
+    } catch (error) {
+      console.log(error);
       return null;
     }
   }
