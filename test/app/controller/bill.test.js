e34b925a2b73ddb6a56e3b36c18983d0490ce663
fxpby <fxpby@outlook.com>
2021-12-07 00:44:29 +0800

    feat: add unit test for list/detail methods in bill controller

--- test/app/controller/bill.test.js
+++ test/app/controller/bill.test.js
@@ -75,4 +75,87 @@ describe('test/app/controller/bill.test.js', () => {
       });
   });
 
+  it('GET /api/bill/list type_id = all 查询账单列表成功', () => {
+    const token = app.jwt.sign({
+      id: '2',
+      username: 'olu-test-1636968127297',
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), // token 有效期为 24 小时
+    }, app.config.jwt.secret);
+
+    return app.httpRequest()
+      .get('/api/bill/list')
+      .set('authorization', token)
+      .send({
+        user_id: 2,
+      }) // post body
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, '200');
+      });
+  });
+
+  it('GET /api/bill/list type_id = 1查询账单列表失败', () => {
+    const token = app.jwt.sign({
+      id: '2',
+      username: 'olu-test-1636968127297',
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), // token 有效期为 24 小时
+    }, app.config.jwt.secret);
+
+    app.mockServiceError('bill', 'list', 'mock user service error');
+
+    return app.httpRequest()
+      .get('/api/bill/list?type_id=1')
+      .set('authorization', token)
+      .send({
+      }) // post body
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, '500');
+      });
+  });
+
+  it('GET /api/bill/detail 查询账单详情成功', () => {
+    const token = app.jwt.sign({
+      id: '2',
+      username: 'olu-test-1636968127297',
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), // token 有效期为 24 小时
+    }, app.config.jwt.secret);
+
+    return app.httpRequest()
+      .get('/api/bill/detail?id=1')
+      .set('authorization', token)
+      .send({
+        user_id: 1,
+      }) // post body
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, '200');
+      });
+  });
+
+  it.only('GET /api/bill/detail 查询账单详情失败', () => {
+    const token = app.jwt.sign({
+      id: '2',
+      username: 'olu-test-1636968127297',
+      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60), // token 有效期为 24 小时
+    }, app.config.jwt.secret);
+
+    app.mockServiceError('bill', 'detail', 'mock user service error');
+
+    return app.httpRequest()
+      .get('/api/bill/detail?id=1')
+      .set('authorization', token)
+      .send({
+        user_id: 1,
+      }) // post body
+      .expect(200)
+      .then(response => {
+        const res = JSON.parse(response.text);
+        assert.equal(res.code, '500');
+      });
+  });
+
 });
